dfaeb8b97197681622d2ad7c4c82df41929168e6
jyotisekhar <jyotisekhar.majumder@oracle.com>
2018-11-12 17:01:21 -0500

    saving vagrant and docker-app config for k8s

---
+++ data/k8s-bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# install minikube which would help in local mode as well automatically creates the ~/.kube/config
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+
+
+#install kubectl
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+
+#install kops
+curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops-linux-amd64
+sudo mv kops-linux-amd64 /usr/local/bin/kops
+
+# install docker
+sudo apt-get install -y docker.io
+sudo apt-get install -y git
+
+sudo usermod -G docker vagrant 
+
+# install python-pip
+sudo apt-get install -y python-pip
+
+#install awscli
+sudo pip install awscli
+
+
